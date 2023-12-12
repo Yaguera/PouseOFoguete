8b5a92a3401cbedb2e12ca86000ab699f44b8d09
Yaguera <ya-gotre@hotmail.com>
2023-12-12 07:50:45 -0300

    ADD: Confettis quando o pouso for bem sucedido

--- script.js
+++ script.js
@@ -40,7 +40,30 @@ let ciclo = () => {
             
             clearInterval(intervalo)
         }else{
-            alert('Pouso bem sucedido')
+            var duration = 3 * 1000;
+            var end = Date.now() + duration;
+
+            (function frame() {
+            // launch a few confetti from the left edge
+            confetti({
+                particleCount: 5,
+                angle: 60,
+                spread: 55,
+                origin: { x: 0 }
+            });
+            // and launch a few from the right edge
+            confetti({
+                particleCount: 5,
+                angle: 120,
+                spread: 55,
+                origin: { x: 1 }
+            });
+
+            // keep going until we are out of time
+            if (Date.now() < end) {
+                requestAnimationFrame(frame);
+            }
+            }());
             clearInterval(intervalo)
         }
         
@@ -51,7 +74,7 @@ let ciclo = () => {
     velocidadeText.innerHTML = "Velocidade: " + Math.abs(velocidade.toFixed(2)) + "m/s";
 }
 
-intervalo = window.setInterval(ciclo, 100)
+intervalo = window.setInterval(ciclo, 80)
 
 
 document.addEventListener('mousedown', ligar);
